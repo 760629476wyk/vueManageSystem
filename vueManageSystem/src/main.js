3e4ad593a1176e7bf56a5e245a88ad1d3e39fea6
760629476wyk <760629476@qq.com>
2019-05-16 16:43:56 +0800

    添加element-ui

--- vueManageSystem/src/main.js
+++ vueManageSystem/src/main.js
@@ -3,9 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// 添加element-ui组件
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
-
+Vue.use(ElementUI)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
